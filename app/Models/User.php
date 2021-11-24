21f8454820151e4ffc77b8de95ccf84e049d819e
chutchapon <chutchapon.soyklum@hotmail.com>
2021-11-24 10:01:10 +0700

    fix User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,14 +32,14 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    // /**
-    //  * The attributes that should be cast.
-    //  *
-    //  * @var array
-    //  */
-    // protected $casts = [
-    //     'email_verified_at' => 'datetime',
-    // ];
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 
     public function Post()
     {
