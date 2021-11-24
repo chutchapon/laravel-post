50f6715af3f3280c60c32a3f011c7e2c3a4897f4
chutchapon <chutchapon.soyklum@hotmail.com>
2021-11-24 11:35:00 +0700

    fix a lot files

--- database/migrations/2021_11_23_030338_create_banners_table.php
+++ database/migrations/2021_11_23_030338_create_banners_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBannerTable extends Migration
+class CreateBannersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateBannerTable extends Migration
      */
     public function up()
     {
-        Schema::create('banner', function (Blueprint $table) {
+        Schema::create('banners', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
             $table->string('name');
@@ -29,6 +29,6 @@ class CreateBannerTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('banner');
+        Schema::dropIfExists('banners');
     }
 }
--- database/migrations/2021_11_23_030434_create_tag_posts_table.php
+++ database/migrations/2021_11_23_030434_create_tag_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagPostTable extends Migration
+class CreateTagPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTagPostTable extends Migration
      */
     public function up()
     {
-        Schema::create('tag_post', function (Blueprint $table) {
+        Schema::create('tag_posts', function (Blueprint $table) {
             $table->id();
             $table->integer('tag_id');
             $table->integer('post_id');
@@ -28,6 +28,6 @@ class CreateTagPostTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tag_post');
+        Schema::dropIfExists('tag_posts');
     }
 }
